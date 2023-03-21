7d942aecaca03f7180bd9f71b9dbabf83ac184d9
Daniel Aguilar <daguilar8749@gmail.com>
2023-03-21 14:52:15 -0700

    fave

--- app/routes.py
+++ app/routes.py
@@ -4,8 +4,20 @@ from flask import render_template
 
 @app.route('/')
 def homePage():
+ 
+    
+    
     return render_template('index.html')
 
-@app.route('/login')
-def loginPage():
-    return render_template('login.html')
+@app.route('/Fave5')
+def fave5Page():
+       fav_artist=[
+        {
+        'artist': 'Fuerza Regida'},
+        {'artist': 'Linkin Park'},
+        {'artist': 'Edgardo Nunez'},
+        {'artist': 'Conejo'},
+        {'artist': 'Junior H'}
+    ]
+    
+       return render_template('fave_5.html', artist=fav_artist)
